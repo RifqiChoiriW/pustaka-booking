651f65d2b31e7974628c31c2d74e17eefb7ba468
RifqiChoiriW <kikichoiri123@gmail.com>
2023-04-06 16:27:44 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rifqi Choiri Wardoyo
+ Saya tinggal di daerah Telaga Pesona
+suporter River Plate";
+ }
+}
